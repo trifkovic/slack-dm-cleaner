af74b8793eda9580f39607b01cf225920a3b0945
trifkovic <nenad@1911.su>
2023-07-09 23:44:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Slack DM Cleaner
+
+A script to delete your messages from a specific DM channel in Slack.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+   ```shell
+   git clone https://github.com/trifkovic/slack-dm-cleaner.git
---
+++ clean.js
@@ -0,0 +1,62 @@
+const { WebClient } = require('@slack/web-api');
+
+const token = '*'; // Replace * with your user OAuth token (USER! not bot!)
+const channelId = '*'; // Replace * with the actual DM Channel ID (Slack browser, open a DM with somebody, last part of the URL https://app.slack.com/client/WORKSPACE_ID/YOUR_DM_ID)
+const userId = '*'; // Replace * with your user ID (Slack browser, click on yourself, last part of the URL https://app.slack.com/client/WORKSPACE_ID/YOUR_USER_ID)
+
+async function deleteDMMessages() {
+  const client = new WebClient(token);
+  const messagesToDelete = [];
+
+  try {
+    let cursor = null;
+
+    // Retrieve conversation history
+    do {
+      const response = await client.conversations.history({
+        channel: channelId,
+        cursor,
+      });
+
+      if (response.ok) {
+        const messages = response.messages;
+
+        // Find user's messages and collect their timestamps
+        for (const message of messages) {
+          if (message.user === userId) {
+            messagesToDelete.push(message.ts);
+          }
+        }
+
+        cursor = response.response_metadata.next_cursor;
+      } else {
+        console.error(response.error);
+        return;
+      }
+    } while (cursor);
+
+    console.log(`Found ${messagesToDelete.length} messages to delete.`);
+
+    // Delete each message by timestamp
+    for (let i = 0; i < messagesToDelete.length; i++) {
+      const timestamp = messagesToDelete[i];
+
+      const result = await client.chat.delete({
+        channel: channelId,
+        ts: timestamp,
+      });
+
+      console.log(`Deleted message with timestamp: ${timestamp}`);
+      console.log(result);
+
+      // Add a delay between each message deletion to adhere to rate limits
+      if (i < messagesToDelete.length - 1) {
+        await new Promise((resolve) => setTimeout(resolve, 1200)); // Wait for 1.2 seconds
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+deleteDMMessages();
